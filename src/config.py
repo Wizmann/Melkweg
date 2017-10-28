6df2999b61bfa0329730fac122b5103e1b2411e8
Wizmann <mail.kuuy@gmail.com>
2017-10-29 00:26:22 +0800

    modify proxy port

--- src/config.py
+++ src/config.py
@@ -10,9 +10,9 @@ fmt = "%(levelname)-8s %(asctime)-15s [%(filename)s:%(lineno)d] %(message)s"
 logging.basicConfig(format=fmt, level=logging.INFO)
 
 SERVER = "127.0.0.1"
-SERVER_PORT = 23456
-CLIENT_PORT = 23457
+SERVER_PORT = 20000
+CLIENT_PORT = 20001
 KEY = "if you stand, if you are true"
 
 SERVER_SOCKS5_ADDR = "127.0.0.1"
-SERVER_SOCKS5_PORT = 23458
+SERVER_SOCKS5_PORT = 20002
