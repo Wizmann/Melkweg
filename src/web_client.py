05464f71b559f5f9d0ff67b79533864ad242a50a
Xilin Sun <s.sn.giraffe@gmail.com>
2017-10-31 00:19:02 -0700

    update web client: remove jupyter notebook comments

--- src/web_client.py
+++ src/web_client.py
@@ -1,46 +1,15 @@
-
 # coding: utf-8
-
-# In[12]:
-
-
 import web
 from web import form
 
-
-# In[13]:
-
-
 import config
 import client
-
-
-# In[8]:
-
 
 render = web.template.render('web_client/')
 urls = (
   '/', 'index'
 )
 app = web.application(urls, globals())
-
-
-# In[ ]:
-
-
-# myform = form.Form( 
-#     form.Textbox("boe"), 
-#     form.Textbox("bax", 
-#         form.notnull,
-#         form.regexp('\d+', 'Must be a digit'),
-#         form.Validator('Must be more than 5', lambda x:int(x)>5)),
-#     form.Textarea('moe'),
-#     form.Checkbox('curly'), 
-#     form.Dropdown('french', ['mustard', 'fries', 'wine'])) 
-
-
-# In[9]:
-
 
 clientconf = form.Form(
     form.Textbox(
@@ -52,18 +21,12 @@ clientconf = form.Form(
         ),
         value = config.CLIENT_PORT),
     form.Button('Start'),
-    form.Button('Kill')
+    # form.Button('Kill')
 )
-
-
-# In[11]:
-
 
 class index: 
     def GET(self): 
         form = clientconf()
-        # make sure you create a copy of the form by calling it (line above)
-        # Otherwise changes will appear globally
         return render.client(form)
 
     def POST(self): 
@@ -75,11 +38,6 @@ class index:
             client.reactor.run()
             return "Success."
 
-
-# In[ ]:
-
-
 if __name__=="__main__":
     web.internalerror = web.debugerror
     app.run()
-
